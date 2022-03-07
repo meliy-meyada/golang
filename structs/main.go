eda0251564404f30129d42b25eb79d49855d0632
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-03-07 10:24:38 +0700

    Add more structs in one keyword

--- structs/main.go
+++ structs/main.go
@@ -1,11 +1,31 @@
 package main
 
+import "fmt"
+
+
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
-	firstName string
+	firstName string 
 	lastName string
+	contact contactInfo
 }
 
 
 func main() {
+	meya := person{
+		firstName: "Meyada",
+		lastName: "Saisan",
+		contact: contactInfo{
+			email: "meyada.info@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	
+	fmt.Printf("%+v", meya)
 
 }
