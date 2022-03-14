d6192ac87efb8ac5e757b7d30a7c1910231d06e7
meliy-meyada <meyada.saisan@gmail.com>
2022-03-14 12:03:58 +0700

    test same result in new solution

--- map/main.go
+++ map/main.go
@@ -5,11 +5,16 @@ import "fmt"
 
 
 func main() {
+	// var colors map[string]string
 
-	colors := map[string]string{
-		"red": "#ff0000",
-		"green": "#4bf745",
 
-	}
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red": "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+	colors["white"] = "#ffffff"
+	
 	fmt.Println(colors)
 }
