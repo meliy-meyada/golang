6814f8580ebe2c79679f9118baa50658c35f616f
meliy-meyada <meyada.saisan@gmail.com>
2022-03-14 12:20:27 +0700

    Create func printMap() and use for loop color, hex in rang c

--- map/main.go
+++ map/main.go
@@ -5,18 +5,18 @@ import "fmt"
 
 
 func main() {
-	// var colors map[string]string
-
-
-	colors := make(map[int]string)
-
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-	colors[10] = "#ffffff"
-	delete(colors, 10)
 	
 
-	fmt.Println(colors)
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
