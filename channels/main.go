49bcae45dde3a06274f8770801919bca2df82203
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-06 13:11:41 +0700

    Fetch requests response result loop

--- channels/main.go
+++ channels/main.go
@@ -19,10 +19,9 @@ func main() {
 	for _, link := range links {
 		go checkLink(link, c)
 	}
-	for i := 0; i < len(links); i++{
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
-
 }
 
 
@@ -31,9 +30,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link+ " might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link + " is up!")
-	c <- "Yup its up"
+	c <- link
 }
